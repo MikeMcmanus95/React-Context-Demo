2e8ee0ab761360c74746415effc17319c33e9e1e
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-03-12 02:21:19 -0400

    initial commit

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BookList = () => {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>Book 1</li>
+        <li>Book 2</li>
+        <li>Book 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
