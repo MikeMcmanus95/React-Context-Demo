ae29c7e44cd727992098deaa7fe96ddc43f4ad17
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-03-13 19:51:07 -0400

    added auth context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <ThemeContext.Consumer>
-      {({ isLightTheme, light, dark }) => {
-        const theme = isLightTheme ? light : dark;
-        return (
-          <nav style={{ background: theme.ui, color: theme.mainColor }}>
-            <h1>Context App</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        );
-      }}
-    </ThemeContext.Consumer>
+    <AuthContext.Consumer>
+      {({ isAuthenticated, toggleAuth }) => (
+        <ThemeContext.Consumer>
+          {({ isLightTheme, light, dark }) => {
+            const theme = isLightTheme ? light : dark;
+            return (
+              <nav style={{ background: theme.ui, color: theme.mainColor }}>
+                <h1>Context App</h1>
+                <div>{isAuthenticated ? 'Logged in' : 'Logged out'}</div>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            );
+          }}
+        </ThemeContext.Consumer>
+      )}
+    </AuthContext.Consumer>
   );
-}
+};
 
 export default Navbar;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -7,8 +7,8 @@ export default class ThemeContextProvider extends Component {
     super();
     this.state = {
       isLightTheme: true,
-      light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
-      dark: { syntax: '#ddd', ui: '#333', bg: '#555' },
+      light: { mainColor: '#555', ui: '#ddd', bg: '#eee' },
+      dark: { mainColor: '#ddd', ui: '#333', bg: '#555' },
     };
   }
 
