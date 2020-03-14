3d6c265c30fcd0bb78ebbebd8aaafee1d6018565
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-03-13 20:22:17 -0400

    refactored booklist to use useContext

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
   return (
-    <ThemeContext.Consumer>
-      {({ isLightTheme, light, dark }) => {
-        const theme = isLightTheme ? light : dark;
-        return (
-          <div
-            className="book-list"
-            style={{ color: theme.mainColor, background: theme.bg }}
-          >
-            <ul>
-              <li style={{ background: theme.ui }}>Book 1</li>
-              <li style={{ background: theme.ui }}>Book 2</li>
-              <li style={{ background: theme.ui }}>Book 3</li>
-            </ul>
-          </div>
-        );
-      }}
-    </ThemeContext.Consumer>
+    <div
+      className="book-list"
+      style={{ color: theme.mainColor, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>Book 1</li>
+        <li style={{ background: theme.ui }}>Book 2</li>
+        <li style={{ background: theme.ui }}>Book 3</li>
+      </ul>
+    </div>
   );
 };
 
